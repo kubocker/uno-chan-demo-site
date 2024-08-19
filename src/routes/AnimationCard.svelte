441411837a89854b39076bf47ae3a1849d6557a2
kubokawa <kubocchi.3511@gmail.com>
2024-08-19 14:54:06 +0900

    fix

--- src/routes/AnimationCard.svelte
+++ src/routes/AnimationCard.svelte
@@ -7,6 +7,7 @@
   <div class="card bg-[#eff6ff] w-80 aspect-[3/2] shadow-xl mx-auto">
     <div class="card-body flex items-center justify-center text-center text-[#1e40af]">
       <div class={`animate-${title}-infinite-1500ms text-red font-20 font-bold text-6xl`}>⚫︎</div>
+      <!-- <div class={`animate-flipOutY-infinite-1500ms text-red font-20 font-bold text-6xl`}>⚫︎</div> -->
     </div>
   </div>
 </div>
