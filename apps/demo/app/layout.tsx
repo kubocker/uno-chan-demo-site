de4e173036fc2c800ed9971a673c28a37af13f7a
kubokawa <kubokawa.work@gmail.com>
2025-06-29 10:34:06 +0900

    feat(demo): make footer fixed to bottom and add bottom padding to main

--- apps/demo/app/layout.tsx
+++ apps/demo/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
-        <main className="flex-1">{children}</main>
+        <main className="flex-1 pb-24">{children}</main>
         <Footer />
       </body>
     </html>
